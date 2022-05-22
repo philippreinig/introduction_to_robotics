23cd96ad9a12e44d891f451c2edcc79eb389be08
PonelKai <ponelkai@gmail.com>
2022-05-22 19:19:48 +0200

    Implemented calculations

--- src/state_estimation/state_estimation/locator.py
+++ src/state_estimation/state_estimation/locator.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import rclpy
 import numpy as np
 from rclpy.node import Node
@@ -18,7 +19,6 @@ class LocatorNode(Node):
         self.get_logger().info('locator node started')
         
     def range_cb(self, msg):
-        self.get_logger().info("Initialising...")
         self.anchor_ranges.append(msg)
         self.anchor_ranges = self.anchor_ranges[-10:]
         if not self.initialized:
@@ -32,14 +32,63 @@ class LocatorNode(Node):
         msg.point.x, msg.point.y, msg.point.z = self.calculate_position()
         msg.header.frame_id = 'world'
         self.position_pub.publish(msg)
+
+
+
+    def get_np_arr_from_point(x,y,z):
+        return np.array([x, y, z])
     
     def calculate_position(self):
         if not len(self.anchor_ranges):
             return 0.0, 0.0, 0.0
         self.get_logger().info("Anchor Ranges: {}".format(self.anchor_ranges))
+
         # YOUR CODE GOES HERE:
-        x = np.mean([r.range for r in self.anchor_ranges]) - 0.5
-        return x, 0.0, 0.0
+
+        # Array: [(msg.range, msg.Point)]
+
+
+        aprox = np.array([1.33213,1.432,1.32167]) # x^
+
+
+        # Residuals: 
+        residual = np.array([])
+
+        for anchor in self.anchor_ranges:
+            # self.get_logger().error("Anchor.anchor: ".format(print(dir(anchor.anchor))))
+            # self.get_logger().info("anchor.anchor.x/y/z: x: {}, y: {}: z: {}".format(anchor.anchor.x, anchor.anchor.y, anchor.anchor.z))
+            pos_anchor = np.array([anchor.anchor.x, anchor.anchor.y, anchor.anchor.z])
+            temp = anchor.range - np.linalg.norm(pos_anchor - aprox)
+            residual = np.append(residual, temp)
+
+
+        res_gradient = np.array([])
+
+        # Gradient des Residuals:
+        for anchor in self.anchor_ranges:
+            temp = np.array([])
+            pos_anchor = np.array([anchor.anchor.x, anchor.anchor.y, anchor.anchor.z])
+            denom = np.linalg.norm(aprox - pos_anchor)
+            if(denom == 0): self.get_logger().error("Zero division. Stuff is scuffed here")
+            for a,b in zip(aprox, pos_anchor):
+                temp = np.append(temp, (a-b)/denom)
+            self.get_logger().info("temp: {}".format(temp))
+            res_gradient = np.append(res_gradient, temp, axis=0)
+
+        res_gradient = res_gradient.reshape(len(self.anchor_ranges), 3)
+
+        self.get_logger().error("res_gradiant: {}".format(res_gradient))
+        
+        # Pseudo-inverse of residual gradient
+        res_gradient_pseudo_inv = np.linalg.pinv(res_gradient)
+        self.get_logger().error("res_gradiant_pseudo_inv: {}".format(res_gradient_pseudo_inv))
+
+        sol = aprox - (res_gradient_pseudo_inv @ residual)
+
+        self.get_logger().error("sol: {}".format(sol))
+
+        return 1.0, 0.0, 0.0
+
 
 
 def main(args=None):
